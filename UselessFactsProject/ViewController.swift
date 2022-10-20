c766a73daea8679c5114f71a5e99dc066f72edbe
Emilly Maia <emillypereira1290@gmail.com>
2022-10-19 21:39:18 -0300

    Initial Commit

---
+++ UselessFactsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UselessFactsProject
+//
+//  Created by Emilly Maia on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
